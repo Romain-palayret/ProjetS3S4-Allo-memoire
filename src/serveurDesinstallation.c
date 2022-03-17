37f75168fc168f73dbb7723f65f4da7ef2e1ac0a
Romain-palayret <81689403+Romain-palayret@users.noreply.github.com>
2022-03-17 15:12:30 +0100

    Create serveurDesinstallation.c

--- src/serveurDesinstallation.c
+++ src/serveurDesinstallation.c
@@ -2,11 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <pthread.h>
 #include <time.h> 
 
 
